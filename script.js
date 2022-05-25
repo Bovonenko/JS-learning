50d949937ebdecb8a8c624c940e0130632ce49c1
Rostyslav <bovonenko@icloud.com>
2022-05-25 14:04:14 +0200

    cycles homeworks 5 exercises

--- script.js
+++ script.js
@@ -1,64 +1,67 @@
 "use strict"; 
 
-//cikles
+//cycles exercises
 
-// let num = 50;
+for (let i = 5; i < 11; i++) {
+    console.log(i);
+}
 
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
-
+// let num = 5;
 // do {
 //     console.log(num);
 //     num++;
 // }
-// while (num < 55);
+// while (num < 11);
 
-// for (let i = 1; i < 10; i++) {
-//     if (i === 6) {
-//         // break;
-//         continue;
-//     }
-//     console.log(i);
+// while (num < 11) {
+//     console.log(num);
+//     num++;
 // }
 
-// for (let i = 0; i < 3; i++) {
-//     console.log(i);
-//     for (let j = 0; j < 3; j++) {
-//         console.log(j);
-//     }
-// }
+for (let i = 20; i > 9; i--) {
+    if (i === 13) {
+        break;
+    }
+    console.log(i);
+}
 
-for (let i = 0; i < 3; i++) {
-    console.log('*');
-    for (let j = 0; j < 3; j++) {
-        console.log(j);
+// secondTask
+
+// console.log(3 % 2);
+// console.log(0 % 2);
+for (let i = 1; i < 11; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
     }
 }
 
-let result = '';
-const length = 7;
 
-for (let i = 1; i < length; i++) {
-
-    for (let j = 0; j < i; j++) {
-        result += '*';
-    }
-
-    result += '\n';
-}
-
-console.log(result);
-
-
-first: for (let i = 0; i < 3; i++) {
-    console.log(`First level: ${i}`);
-    for (let j = 0; j < 3; j++) {
-        console.log(`Second level: ${j}`);
-        for (let k = 0; k < 3; k++) {
-            if (k === 2) continue first;
-            console.log(`Third level: ${k}`);
+// thirdTask
+for (let i = 2; i <= 16; i++) {
+        if (i % 2 === 0) {
+            continue;
+        } else {
+            console.log(i);
         }
     }
+
+
+// fourthTask   
+let i = 2;
+while (i <= 16) {
+    if (i % 2 === 0) {
+        i++;
+        continue;
+    } else {
+        console.log(i);
+    }
+    i++;
 }
+
+// fifthTask
+const arrayOfNumbers = [];
+
+for (let i = 5; i < 11; i++) {
+    arrayOfNumbers[i - 5] = i;
+}
+console.log(arrayOfNumbers);
