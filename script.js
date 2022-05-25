4ca153e4ed788832164afc4f7dca5b4dfe80d7bd
Rostyslav <bovonenko@icloud.com>
2022-05-25 11:19:27 +0200

    compared and modified my FilmApp

--- script.js
+++ script.js
@@ -2,19 +2,23 @@
 
 //first console app
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?'),
+const numberOfFilms = + prompt('Сколько фильмов вы уже посмотрели?'),
     personalMovieDB = {
         'count': numberOfFilms,
         'movies': {},
         'actors': {},
         'genres': [],
         'privat': false
-    },
-    lastWatchedFilm = prompt('Один из последних просмотренных фильмов?'),
-    rateOfLastWatchedFilm = prompt('На сколько оцените его?');
+    };
 
-    personalMovieDB['movies'] = `${lastWatchedFilm}: ${rateOfLastWatchedFilm}`;
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
   
-    console.log(personalMovieDB);
+console.log(personalMovieDB);
 
 
