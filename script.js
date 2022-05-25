262e55b5e5ec337e560a0fff3c1f3e3948c83ff1
Rostyslav <bovonenko@icloud.com>
2022-05-25 12:31:37 +0200

    learning logical operators

--- script.js
+++ script.js
@@ -1,37 +1,64 @@
 "use strict"; 
 
-//conditions
+//logical operators
 
-if (4 == 9) {
-    console.log('Ok!');
-} else {
-    console.log('Error!');
-}
+// const hamburger = 5;
+// const fries = 0;
 
-
-
-// if (num < 49) {
-//     console.log('Error');
-// } else if (num > 100) {
-//     console.log('Too much');
+// if (hamburger && fries) {
+//     console.log('I am not hungry!');
 // } else {
-//     console.log('Ok!');
+//     console.log('Not enough!');
 // }
 
-// (num === 50) ? console.log("Ok!") : console.log('Error');
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
 
-const num = 51;
-switch (num) {
-    case 49: 
-        console.log('Error');
-        break;
-    case 100: 
-        console.log('Error');
-        break;
-    case 50: 
-        console.log('OK');
-        break;
-    default: 
-        console.log('Not this time!');
-        break;
+// console.log(hamburger === 3 && cola && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'adsafaf');
+
+// if (hamburger === 3 && cola ===1 && fries) {
+//     console.log('Evrn is not hungry!');
+// } else {
+//     console.log('Not enough!');
+// }
+
+// console.log((hamburger && fries));
+
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+
+if (hamburger && cola || fries ===3 && nuggets) {
+    console.log('Evrn is happy!');
+} else {
+    console.log('Not enough!');
 }
+
+console.log((hamburger ===3 && cola === 2 || fries ===3 && nuggets));
+
+let johnReport, alexReport, samReport = 'done';
+
+console.log(johnReport || alexReport || samReport);
+
+console.log(!0);
+
+
+console.log(NaN || 2 || undefined);
+console.log(NaN && 2 && undefined);
+console.log(1 && 2 && 3);
+console.log(!1 && 2 || !3);
+console.log(25 || null && !3);
+console.log(NaN || null || !3 || undefined || 5);
+console.log(NaN || null && !3 && undefined || 5);
+console.log(5 === 5 && 3 > 1 || 5);
+
