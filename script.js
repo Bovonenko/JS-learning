bb50582155340e4dd9666c8b27617f6fd21e7e22
Rostyslav <bovonenko@icloud.com>
2022-05-25 11:34:18 +0200

    learning conditions

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 "use strict"; 
 
-//first console app
+//conditions
 
-const numberOfFilms = + prompt('Сколько фильмов вы уже посмотрели?'),
-    personalMovieDB = {
-        'count': numberOfFilms,
-        'movies': {},
-        'actors': {},
-        'genres': [],
-        'privat': false
-    };
-
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-  
-console.log(personalMovieDB);
+if (4 == 9) {
+    console.log('Ok!');
+} else {
+    console.log('Error!');
+}
 
 
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Too much');
+// } else {
+//     console.log('Ok!');
+// }
+
+// (num === 50) ? console.log("Ok!") : console.log('Error');
+
+const num = 51;
+switch (num) {
+    case 49: 
+        console.log('Error');
+        break;
+    case 100: 
+        console.log('Error');
+        break;
+    case 50: 
+        console.log('OK');
+        break;
+    default: 
+        console.log('Not this time!');
+        break;
+}
