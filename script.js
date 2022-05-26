3a2574330d96387a570e1ec70aa72b81acb644b9
Rostyslav <bovonenko@icloud.com>
2022-05-26 10:54:47 +0200

    cycles homework ** pyramid

--- script.js
+++ script.js
@@ -27,11 +27,22 @@
 
 
 //thirdTask
-const data = [5, 10, 'Shopping', 20, 'Homework'];
-const result = [];
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// const result = [];
 
-for (let i = 1; i <= data.length; i++) {
-    result[i - 1] = data[data.length - i];
+// for (let i = 1; i <= data.length; i++) {
+//     result[i - 1] = data[data.length - i];
+// }
+
+// console.log(result);
+
+const lines = 5;
+let result = '';
+
+for (let i = 1; i <= lines; i++) {
+    for (let j = 0; j < i; j++) {
+        result += '*';
+    }
+    result += '\n';
 }
-
 console.log(result);
