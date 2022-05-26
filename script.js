89ae55710b2ead2eb176b66fa416faee2c6d3a45
Rostyslav <bovonenko@icloud.com>
2022-05-26 16:20:09 +0200

    methods

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 "use strict";
 
-const str = "test";
+const str = "tEst";
 
-console.log(str.length);
+// console.log(str[2] = 'd');
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+let fruit = "Some fruit";
+
+console.log(fruit.indexOf("q"));
+
+const logg = "Hello world";
+
+console.log(logg.slice(-5, -1));
+
+console.log(logg.substring(6, 11));
+console.log(logg.substr(6, 3));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
