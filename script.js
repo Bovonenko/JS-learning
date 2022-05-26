1ab68d8592b6d0bcf5c5116c9a7e970476e50020
Rostyslav <bovonenko@icloud.com>
2022-05-26 16:18:59 +0200

    methods

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
 "use strict";
 
-function sayHello(name) {
-    return(`Hello ${name}!`);
-}
-sayHello('Rostyslav');
+const str = "test";
 
-function returnNeighboringNumbers(num) {
-    return [num - 1, num, num + 1];
-}
-returnNeighboringNumbers(3);
-
-function getMathResult(num, times) {
-    if (typeof(num) !== 'number' && num <= 0) {
-        return num;
-    }
-    let str = '';
-
-    for (let i = 1; i <= times; i++) {
-        str += num * i;
-        if (i === times)  {
-            str += `${num * i}`;
-        } else {
-            str += `${num * i}---`;
-        }
-    }
-    return str;
-}
-getMathResult(3, '2');
+console.log(str.length);
