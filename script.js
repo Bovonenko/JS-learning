7585cff560c7f3c95510bbe7e43ad38986737a4d
Rostyslav <bovonenko@icloud.com>
2022-05-27 10:30:25 +0200

    fibonachi task my version

--- script.js
+++ script.js
@@ -1,68 +1,23 @@
 "use strict";
 
-function getTimeFromMinutes(minutesTotal) {
-    // if (typeof(time) != 'number' || !Number.isInteger(time) || time < 0) {
-    //      console.log('Ошибка, проверьте данные');
-    // } else {
-    //     let decimal = time / 60;
-    //     let hours = Math.floor(decimal),
-    //         minutes = Math.round(( decimal - Math.floor(decimal) ) * 60);
-
-    //     if (hours == 1) {
-    //          console.log(`Это ${hours} час и ${minutes} минут`);
-    //     } else if (hours === 0 || hours >= 5) {
-    //          console.log(`Это ${hours} часов и ${minutes} минут`);
-    //     } else {
-    //          console.log(`Это ${hours} часа и ${minutes} минут`);
-    //     }
-    // }
-
-if (typeof(minutesTotal) !== 'number' || minutesTotal < 0 || !Number.isInteger(minutesTotal)) {
-        return "Ошибка, проверьте данные";
+function fib(length) {
+    let arr = [0, 1];
+    let str = '0 1';    
+    if (typeof(length) !== 'number' || length <= 0 || !Number.isInteger(length)) {
+        return '';
     }
-
-    const hours = Math.floor(minutesTotal / 60);
-    const minutes = minutesTotal % 60;
-
-    let hoursStr = '';
-
-    switch (hours) {
-        case 0: 
-            hoursStr = 'часов';
-            break;
+    switch (length) {
         case 1:
-            hoursStr = 'час';
-            break;
-        case 2:
-        case 3:
-        case 4:
-            hoursStr = 'часа';
+            str = '0';
             break;
         default:
-            hoursStr = 'часов';
+            for (let i = 2; i < length; i++) {
+                arr[i] = arr[i - 1] + arr[i - 2];
+                
+                str += ` ${arr[i]}`;
+            }
+            break;
     }
-
-    return `Это ${hours} ${hoursStr} и ${minutes} минут`;
+    return str;
 }
-getTimeFromMinutes(100);
-
-function findMaxNumber(a, b, c, d) {
-    // const arr = [a, b, c, d];
-    // for (let i = 0; i < arr.length; i++) {
-    //     if (!arr[i] || typeof(arr[i]) != 'number') {
-    //         return 0;
-    //     }
-    // }
-    // return Math.max.apply(null, arr);
-    if (typeof(a) !== 'number' ||
-        typeof(b) !== 'number' ||
-        typeof(c) !== 'number' ||
-        typeof(d) !== 'number') {
-        return 0;
-    } else {
-        return Math.max(a, b ,c, d);
-    }
-}
-findMaxNumber(7, 1, 33.3, '10');
-
-console.log(150 % 60);
+fib(9);
