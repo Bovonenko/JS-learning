5008e051dd53678411b6462def29923372334a1d
Rostyslav <bovonenko@icloud.com>
2022-05-27 11:25:39 +0200

    objects

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 "use strict";
 
-function first() {
-    //Do something
-    setTimeout (function() {
-        console.log(1);
-    }, 500);
-}
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
+options.makeTest();
 
-function second() {
-    console.log(2);
-}
-first();
-second();
+const {border, bg} = options.colors;
+console.log(border);
 
-function learnJS(lang, callback) {
-    console.log(`I learn: ${lang}`);
-    callback();
-}
-function done() {
-    console.log('I have done this lesson!');
-}
-learnJS('JavaScript', done);
+// console.log(Object.keys(options).length);
+
+// console.log(options[colors]['border']);
+// delete options.name;
+// console.log(options);
+
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     }
+// }
+// console.log(counter);
