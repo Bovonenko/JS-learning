1013a14b0df1c4b0ca2ab64c09bf468f02942246
Rostyslav <bovonenko@icloud.com>
2022-05-28 12:37:59 +0200

    array showFamily standardizeStrings apps exercise

--- script.js
+++ script.js
@@ -1,44 +1,30 @@
 "use strict";
 
-const personalPlanPeter = {
-    name: "Peter",
-    age: "29",
-    skills: {
-        languages: ['ru', 'eng'],
-        programmingLangs: {
-            js: '20%',
-            php: '10%'
-        },
-        exp: '1 month'
-    },
-    showAgeAndLangs: function(plan) {
-        const {age} = plan;
-        const {languages} = plan.skills;
-        let str = (`Мне ${age} и я владею языками: `);
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+const arra = [];
 
-        languages.forEach(function(item) {
-            str += `${item.toUpperCase()} `;
-        });
-       console.log(str);
+function showFamily(arr) {
+    let str = '';
+    if (arr.length === 0) {
+        console.log('Семья пуста');
     }
-};
-
-personalPlanPeter.showAgeAndLangs(personalPlanPeter);
-console.log(Object.keys(personalPlanPeter).length);
-
-// function showExperience(plan) {
-//     console.log(plan['skills']['programmingLangs'].js);
-// }
-
-// showExperience(personalPlanPeter);
-// if (key + 1 === Object.keys(plan['skills']['programmingLangs']).length) {
-//             langs += `Язык ${key} изучен на ${plan['skills']['programmingLangs'][key]}`;
-//         }
-function showProgrammingLangs(plan) {
-    let langs = '';
-    for (let key in plan['skills']['programmingLangs']) {
-        langs += `Язык ${key} изучен на ${plan['skills']['programmingLangs'][key]}\n`;
-    }
-    return langs;
+    arr.forEach(function(name) {
+        str += `${name} `;
+    });
+    console.log(`Семья состоит из: ${str}`);
 }
-showProgrammingLangs(personalPlanPeter);
+showFamily(arra);
+
+const cities = ['Lisbon', 'Rome', 'Milan', 'Dublin'];
+function standardizeStrings(arr) {
+    let str = '';
+    arr.forEach(function(city) {
+        console.log(city.toLowerCase());
+    });
+}
+standardizeStrings(cities);
+
+
+
+
+// console.log(arr.length);
