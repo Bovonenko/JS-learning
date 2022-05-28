bfd9659b3b8ff9b5279f11188043a7c9a9ba5654
Rostyslav <bovonenko@icloud.com>
2022-05-28 12:16:24 +0200

    objects exerciese

--- script.js
+++ script.js
@@ -10,11 +10,35 @@ const personalPlanPeter = {
             php: '10%'
         },
         exp: '1 month'
+    },
+    showAgeAndLangs: function(plan) {
+        const {age} = plan;
+        const {languages} = plan.skills;
+        let str = (`Мне ${age} и я владею языками: `);
+
+        languages.forEach(function(item) {
+            str += `${item.toUpperCase()} `;
+        });
+       console.log(str);
     }
 };
 
-function showExperience(plan) {
-    return plan['skills']['exp'];
-}
+personalPlanPeter.showAgeAndLangs(personalPlanPeter);
+console.log(Object.keys(personalPlanPeter).length);
 
-showExperience(personalPlanPeter);
+// function showExperience(plan) {
+//     console.log(plan['skills']['programmingLangs'].js);
+// }
+
+// showExperience(personalPlanPeter);
+// if (key + 1 === Object.keys(plan['skills']['programmingLangs']).length) {
+//             langs += `Язык ${key} изучен на ${plan['skills']['programmingLangs'][key]}`;
+//         }
+function showProgrammingLangs(plan) {
+    let langs = '';
+    for (let key in plan['skills']['programmingLangs']) {
+        langs += `Язык ${key} изучен на ${plan['skills']['programmingLangs'][key]}\n`;
+    }
+    return langs;
+}
+showProgrammingLangs(personalPlanPeter);
