f31b1f905dfc2d49d2a84570fbc15d7bd5ef2438
Rostyslav <bovonenko@icloud.com>
2022-05-30 20:37:06 +0200

    debug exercise

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
 "use strict";
-// let number = 5; debugger
-// function logNumber() {
-//     console.log(number); debugger
-// }
 
-// number = 6;
+const restorantData = { 
+    menu: [ 
+        {
+            name: 'Salad Caesar',
+            price: '14$'
+        },
+        {
+            name: 'Pizza Diavola',
+            price: '9$'
+        },
+        {
+            name: 'Beefsteak',
+            price: '17$'
+        },
+        {
+            name: 'Napoleon',
+            price: '7$'
+        }
+    ],
+    waitors: [
+        {name: 'Alice', age: 22}, {name: 'John', age: 24}
+    ],
+    averageLunchPrice: '30$',
+    openNow: true
+};
 
-// logNumber(); debugger
+function transferWaitors(data) {debugger
+    
+    const copy = JSON.parse(JSON.stringify(data));
 
-// number = 8 ;
-
-// logNumber(); debugger
-
-function createCounter() {
-    let counter = 0;
-
-    const myFunction = function() {debugger
-        counter = counter + 1;debugger
-        return counter;debugger
-    };  
-
-    return myFunction;
+    copy.waitors[0] = {name: 'Mike', age: 32};debugger
+    return copy;
 }
 debugger
-const increment = createCounter();debugger
-const c1 = increment();debugger
-const c2 = increment();debugger
-const c3 = increment();debugger
+transferWaitors(restorantData);
 
-console.log(c1, c2, c3);
+
+// let prc = '14$';
+// let prce = '14$';
+// console.log(+prc.slice(0, -1) + 2);
+// // function isOpen(prop) {
+// //     let answer = '';
+// //     answer = prop.openNow ? 'Открыто' : 'Закрыто';
+
+// //     return answer;
+// // }
+
+// // console.log(isOpen(restorantData));
+
+// function isAverageLunchPriceTrue(fDish, sDish, average) {
+//     console.log(+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)));
+//     if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < average) {
+//         return 'Цена ниже средней';
+//     } else {
+//         return 'Цена выше средней';
+//     }
+// }
+
+// console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
