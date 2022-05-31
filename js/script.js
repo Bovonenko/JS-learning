6a37f0d3c670cad558b64de3a4ca0289c5a2712a
Rostyslav <bovonenko@icloud.com>
2022-05-31 16:16:30 +0200

    DOM, data-атрибуты,  for/of

--- js/script.js
+++ js/script.js
@@ -1,29 +1,23 @@
-'use strict';
+// console.log(document.head
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastChild);
 
-const btns = document.querySelectorAll('button'),
-      overlay = document.querySelector('.overlay');
 
-// let i = 0;
-const deleteElement = (e) => {
-    console.log(e.target);
-    console.log(e.type);
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('#current').parentElement);
 
-    // i++;
-    // if (i == 1) {
-    //     btn.removeEventListener('click', deleteElement);
-    // }
 
-};
-// btn.addEventListener('click', deleteElement);
-// overlay.addEventListener('click', deleteElement);
+// console.log(document.querySelector('[data-current="3"]').nextSibling);
+// console.log(document.querySelector('[data-current="3"]').previousSibling);
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+// console.log(document.querySelector('[data-current="3"]').previousElementSibling);
 
-btns.forEach(btn => {
-    btn.addEventListener('click', deleteElement, {once: true});
-});
-
-const link = document.querySelector('a');
-link.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    console.log(event.target);
-});
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+    console.log(node);
+}
