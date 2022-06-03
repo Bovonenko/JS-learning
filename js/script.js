86145d9210dca4a8c6a9dd2cae842c2707f9b323
Rostyslav <bovonenko@icloud.com>
2022-06-03 09:49:29 +0200

    script defer async

--- js/script.js
+++ js/script.js
@@ -1,33 +1,14 @@
-//touchstart
-//touchmove
-//touchend
-//touchenter
-//touchleave
-//touchcancel
+"use strict";
 
-window.addEventListener('DOMContentLoaded', () => {
-    const box = document.querySelector('.box');
+const p = document.querySelectorAll('p');
+console.log(p);
 
-    box.addEventListener('touchstart', (e) => {
-        e.preventDefault();
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.body.append(script);
+}
 
-        console.log('start');
-        console.log(e.targetTouches);
-    });
-
-    box.addEventListener('touchmove', (e) => {
-        e.preventDefault();
-
-        console.log(e.targetTouches[0].pageX);
-    });
-
-    // box.addEventListener('touchend', (e) => {
-    //     e.preventDefault();
-
-    //     console.log('End');
-    // });
-});
-
-//touches 
-//targetTouches
-//changedTouches
+loadScript("js/test.js");
+loadScript("js/some.js");
