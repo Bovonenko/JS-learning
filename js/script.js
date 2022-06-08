1491f5afa9f62b5795e65e05463ca69bcc618ed0
Rostyslav <bovonenko@icloud.com>
2022-06-08 19:42:20 +0200

    Функция-конструктор

--- js/script.js
+++ js/script.js
@@ -1,26 +1,40 @@
 "use strict";
 
-const now = new Date('2022-06-06');
-// new Date.parse('2022-06-06');
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
 
-console.log(now.setHours(40));
-console.log(now);
-
-// console.log(now.getFullYear());
-// console.log(now.getMonth());
-// console.log(now.getDate());
-// console.log(now.getHours());
-// console.log(now.getUTCHours());
-
-// console.log(now.getTimezoneOffset());
-// console.log(now.getTime());
-
-let start = new Date();
-
-for (let i = 0; i < 100000; i++) {
-    let some = i ** 3;
+    this.hello = function() {
+        console.log(`Hello ${this.name}`);
+    };
 }
 
+class User {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+        this.human = true;
+    }
+    hello() {
+        console.log(`Hello! ${this.name}`);
+    }
+    exit() {
+        console.log(`User ${this.name} left`);
+    }
+}
 
-let end = new Date();
-alert(`Цикл отработал за ${end - start} миллисекунд`);
+User.prototype.exit = function() {
+    console.log(`User ${this.name} left`);
+};
+
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
+
+ivan.exit();
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
