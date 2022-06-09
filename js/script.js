300d6fa07e29ff683b8990f8cd146233d2657d1a
Rostyslav <bovonenko@icloud.com>
2022-06-09 12:06:11 +0200

    CLASS

--- js/script.js
+++ js/script.js
@@ -1,77 +1,36 @@
-"use strict";
+'use strict';
 
-// function showThis(a, b) {
-//     console.log(this);
-//     function sum() {
-//         console.log(this);
-//         return a + b;
-//     }
-
-//     console.log(sum());
-// }
-// showThis(4, 5);
-
-// const obj = {
-//     a: 20,
-//     b: 15,
-//     sum: function() {
-//         function shout() {
-//             console.log(this);
-//         }
-//         shout();
-//     }
-// };
-// obj.sum();
-
-// function User(name, id) {
-//     this.name = name;
-//     this.id = id;
-//     this.human = true;
-//     this.hello = function() {
-//         console.log('Hello' + this.name);
-//     };
-// }
-// let ivan = new User('Ivan', 23);
-
-// function sayName(surname) {
-//     console.log(this);
-//     console.log(this.name + surname);
-// }
-
-// const user = {
-//     name: 'John'
-// };
-
-// sayName.call(user, 'Smith');
-// sayName.apply(user, ['Smith']);
-
-// function count(num) {
-//     return this*num;
-// }
-// const double = count.bind(2);
-// console.log(double(3));
-// console.log(double(13));
-
-const btn = document.querySelector('button');
-
-btn.addEventListener('click', (e) => {
-    // console.log(this);
-    e.target.style.backgroundColor = 'red';
-});
-
-const obj = {
-    num: 5,
-    sayNumber: function() {
-        const say = () => {
-            console.log(this.num);
-        };
-
-        say();
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
     }
-};
 
-obj.sayNumber(); 
+    calcAre() {
+        return this.height * this.width;
+    }
+}
 
-const double = (a, b) => a * 2;
+class ColoredRectangleWidthText extends Rectangle {
+    constructor(height, width, text, bgColor ) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
 
-console.log(double(5));
+    showMyProps() {
+        console.log(`Text: ${this.text}, color: ${this.bgColor}`);
+    }
+}
+
+const div = new ColoredRectangleWidthText(25, 10, 'Hello World', 'red');
+
+div.showMyProps();
+console.log(div.calcAre());
+
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(20, 100);
+
+// console.log(long.calcAre());
+// console.log(square.calcAre());
+
